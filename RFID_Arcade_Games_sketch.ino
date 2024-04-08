e5283eadbfaf8925360e01fb4458ee27fd3c184e
ramandeepkau <64877434+ramandeepkau@users.noreply.github.com>
2024-04-08 19:58:35 +1200

    Add files via upload

---
+++ RFID_Arcade_Games_sketch.ino
@@ -0,0 +1,8 @@
+#include <SPI.h>
+#include <RFID.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define GREEN_LED_PIN 4  // Set to the actual pin number for the green LED
+#define RED_LED_PIN 5    // Set to the actual pin number for the red LED
+
