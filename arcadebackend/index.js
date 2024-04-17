f26bf8ced289aa7a684720bb71ffb58d9b2ad16e
Sasnz <r.koks@hotmail.co.nz>
2024-04-17 14:50:48 +1200

    removed websocket, amended issues and got apps to launch

--- arcadebackend/index.js
+++ arcadebackend/index.js
@@ -1,20 +1,19 @@
 import express, {json} from "express"
 import { exec } from "child_process"
-
+import cors from "cors"
 const app = express()
 
 const PORT = 3000
 
 app.use(json())
+app.use(cors());
 
 app.post('/executeShortcut', (req, res) => {
     // Replace 'path_to_your_shortcut.lnk' with the path to your shortcut file
     const { path } = req.body
 
-    const shortcutPath = `C:/Users/GGPC/Desktop/${path}`;
-
     // Execute the shortcut
-    exec(shortcutPath, (error, stdout, stderr) => {
+    exec(path, (error, stdout, stderr) => {
         if (error) {
         console.error(`Error executing shortcut: ${error}`);
         res.status(500).send('Error executing shortcut');
