6f4a87fa8f4d5ade316d3e7984b133eb1252e07b
Sasnz <r.koks@hotmail.co.nz>
2024-05-02 16:22:10 +1200

    new branch ready for css

--- react-arcade/src/App.js
+++ react-arcade/src/App.js
@@ -3,30 +3,42 @@ import "./App.css";
 import Button from "./components/button";
 
 function App() {
-
-  let temp = [{game: "PacMan", path: "react-arcade/assets/games/BadPac.exe"},
-              {game: "NotPacMan", path:"NoWhere"}]
+  let temp = [
+    { game: "PacMan", path: "react-arcade/assets/games/BadPac.exe" },
+    { game: "Test 2", path: "NoWhere" },
+    { game: "Test 3", path: "NoWhere" },
+    { game: "Test 4", path: "NoWhere" },
+    { game: "Test 5", path: "NoWhere" },
+    { game: "Test 6", path: "NoWhere" },
+    { game: "Test 7", path: "NoWhere" },
+  ];
 
   return (
     <div className="App">
       <h1>The Otago Polytechnic Arcade</h1>
-      <div class="gameSelect">
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-          {temp.map(game => (
+
+      <div class="gameTile">
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {temp.map((game) => (
+            <Carousel>
             <li key={game.game}>
-              <Button game={game.game} path={game.path}/>
+              <Button game={game.game} path={game.path} />
             </li>
+            </Carousel>
           ))}
         </ul>
+      </div>
+      {/* End of gameTile */}
 
-        <div class="bottomNav">
-          <div class="backButton">
-              <button type="button">Home</button>
-          </div>
+      <div class="bottomNav">
+        <div class="backButton">
+          <button type="button">Home</button>
         </div>
       </div>
+      {/* End of bottomNav */}
     </div>
   );
+
 }
 
 export default App;
