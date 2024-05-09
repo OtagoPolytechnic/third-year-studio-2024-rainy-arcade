c21db0d7712aa2230ef6f0c8fc8ad072d81cc713
Samantha Craig (1000048108) <CRAISR1@student.op.ac.nz>
2024-05-10 11:17:08 +1200

    Working script

--- controller-select/load_profile.py
+++ controller-select/load_profile.py
@@ -16,25 +16,25 @@ def load_profile(input_device):
             print(f"Error executing command: {e}")
     elif input_device.lower() == "keyboard":
         # Load keyboard1 profile onto joystick 1 and keyboard2 profile onto joystick 2
-        command = [antimicrox_path,  "--tray", "--profile", keyboard1_profile_path, "--profile-controller", "1"]
-        command1 = [antimicrox_path, "--unload", "2", "--tray", "--profile", keyboard2_profile_path, "--profile-controller", "2"]
-        command2 = [antimicrox_path, "--profile", keyboard2_profile_path, "--profile-controller", "2"]
+        command = [antimicrox_path, "--unload", "1", "--tray"]
+        command1 = [antimicrox_path, "--profile-controller", "1", "--profile", keyboard1_profile_path]
+        command2 = [antimicrox_path, "--unload", "2", "--tray"]
+        command3 = [antimicrox_path, "--profile-controller", "2", "--profile", keyboard2_profile_path]
         try:        
             subprocess.Popen(command)
-            time.sleep(5)
-            # Terminate the first instance of AntiMicroX
-            kill_antimicrox()
             time.sleep(3)
             subprocess.Popen(command1)
             time.sleep(3)
-            subprocess.Popen(command2)
-            #subprocess.run(command, check=True)
+            subprocess.Popen(command2)            
+            # Terminate the first instance of AntiMicroX
+            kill_antimicrox()
+            time.sleep(3)
+            subprocess.Popen(command3)
         except Exception as e:
             print(f"Error executing command: {e}")
     else:
         print("Invalid input device. Please choose 'Mouse' or 'Keyboard'.")
         return
-
 
 def kill_antimicrox():
     # Command to kill AntiMicroX process
