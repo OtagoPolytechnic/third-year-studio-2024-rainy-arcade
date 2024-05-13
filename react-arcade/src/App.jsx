4549ecfad3dc94da9a900d3bc1099bd4c0df05a7
Sasnz <r.koks@hotmail.co.nz>
2024-05-13 19:03:11 +1200

    added scss to app.jsx

--- react-arcade/src/App.jsx
+++ react-arcade/src/App.jsx
@@ -1,6 +1,9 @@
 import Game from "./components/Game";
 import React from "react";
 import Carousel from "./components/Carousel";
+import "../src/components/App.scss";
+
+
 
 const App = () => {
   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
