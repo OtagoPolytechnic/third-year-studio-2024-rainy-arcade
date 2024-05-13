1309a102191d1b2e95ee2323e87fb03645fc4170
Samantha Craig (1000048108) <CRAISR1@student.op.ac.nz>
2024-05-13 15:19:52 +1200

    Made antimicro launch hidden

--- controller-select/load_profile.py
+++ controller-select/load_profile.py
@@ -9,17 +9,17 @@ keyboard2_profile_path = r"keyboard2.gamecontroller.amgp"
 
 def load_profile(input_device):
     if input_device.lower() == "mouse":
-        mouseCommand = [antimicrox_path, "--tray", "--profile", mouse_profile_path]
+        mouseCommand = [antimicrox_path, "--hidden", "--profile", mouse_profile_path]
         try:        
             subprocess.run(mouseCommand, check=True)
         except Exception as e:
             print(f"Error executing command: {e}")
     elif input_device.lower() == "keyboard":
         # Load keyboard1 profile onto joystick 1 and keyboard2 profile onto joystick 2
-        command = [antimicrox_path, "--unload", "1", "--tray"]
+        command = [antimicrox_path, "--unload", "1", "--hidden"]
         command1 = [antimicrox_path, "--profile-controller", "1", "--profile", keyboard1_profile_path]
-        command2 = [antimicrox_path, "--unload", "2", "--tray"]
-        command3 = [antimicrox_path, "--profile-controller", "2", "--profile", keyboard2_profile_path]
+        command2 = [antimicrox_path, "--unload", "2"]
+        command3 = [antimicrox_path, "--hidden", "--profile-controller", "2", "--profile", keyboard2_profile_path]
         try:        
             subprocess.Popen(command)
             time.sleep(3)
