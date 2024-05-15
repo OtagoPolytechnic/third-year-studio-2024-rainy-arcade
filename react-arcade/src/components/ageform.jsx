f5a324fdf94d770e51421383f7c92c5de78bebc8
Sasnz <r.koks@hotmail.co.nz>
2024-05-15 13:59:20 +1200

    styling access for age check

--- react-arcade/src/components/ageform.jsx
+++ react-arcade/src/components/ageform.jsx
@@ -1,7 +1,7 @@
 import { getGames } from "../utils/Arcade-Backend-Functions"
 const Ageform = ({setGames, setPath}) => { 
     return(
-        <div>
+        <div className="ageCheck">
             <h2>Over 18</h2>
             <button onClick={() => getGames(setGames, setPath, true)}>Yes</button>
             <button onClick={() => getGames(setGames, setPath, false)}>No</button>
