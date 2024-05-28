9e1ce5502d2d6ef79485ff848874e021fda9c9e0
ramandeepkau <64877434+ramandeepkau@users.noreply.github.com>
2024-05-28 14:08:58 +1200

    Added student id for secirity

---
+++ lock_unlock_feature.ino
@@ -0,0 +1,59 @@
+#include <Keyboard.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 5
+#define KEY_RETURN 0xB0  // The hex value for the return key is 0xB0.
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+char Enter = KEY_RETURN;  // Return key is declared as Enter.
+String readid;
+String card1 = "3A3D9D9E";  // Change this value to the UID of your card.
+
+void setup() {
+  Serial.begin(9600);
+  Keyboard.begin();
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Setup complete. Scan your RFID tag.");
+}
+
+void temp(byte *buffer, byte bufferSize) {
+  // Function to store card UID as a string datatype.
+  readid = "";
+  for (byte i = 0; i < bufferSize; i++) {
+    readid = readid + String(buffer[i], HEX);
+  }
+  readid.toUpperCase(); // Ensure the UID is in uppercase
+  Serial.print("Card UID: ");
+  Serial.println(readid);
+}
+
+void loop() {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));  // Display card details in serial monitor.
+  temp(mfrc522.uid.uidByte, mfrc522.uid.size);
+  if (readid == card1) {
+    Serial.println("Card recognized. Sending unlock sequence.");
+    Keyboard.press(KEY_LEFT_GUI);  // Press the left Windows key.
+    Keyboard.press('l');           // Press the "l" key.
+    Keyboard.releaseAll();         // Release all keys.
+    delay(100);
+    Keyboard.press(Enter);         // Press the Enter key.
+    Keyboard.release(Enter);       // Release the Enter key.
+    delay(100);
+    Keyboard.print("23990");       // Change this value to your Windows PIN/Password.
+    Keyboard.releaseAll();
+    delay(100);
+    Keyboard.press(Enter);
+    Keyboard.releaseAll();
+  } else {
+    Serial.println("Unrecognized card.");
+  }
+}
